9dffcf5f3380d55c955836858cfe5b2d37e86a75
Henrik Nyman <henrik.nyman@optofidelity.com>
2018-07-12 22:37:57 +0300

    Add work experience

--- content.py
+++ content.py
@@ -5,6 +5,8 @@ Welcome to www.nyymanni.com
 Size of this webpage is 68 kB, of which 56 kB is due to  the background image.
 I like small websites.
 """
+import threading
+import time
 
 
 class Self:
@@ -16,6 +18,13 @@ class Self:
         year = 2011
         yield Matriculation(location='Euran Lukio')
 
+        # Military service
+        year = 2012
+        time.sleep(180 * 24 * 3600)
+
+        year = 2015
+        threading.Thread(target=self.work_experience).start()
+
         year = 2016
         yield Candidate(major='Information Technology',
                         location='Tampere University of Technology')
@@ -23,3 +32,8 @@ class Self:
         year = 2018
         yield MasterOfScience(major='Pervasive Systems',
                               location='Tampere University of Technology')
+
+    def work_experience(self):
+
+        company = 'OptoFidelity Ltd.'
+        title = 'Software Engineer'
